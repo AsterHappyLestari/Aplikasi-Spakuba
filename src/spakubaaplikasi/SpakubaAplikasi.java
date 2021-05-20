46cbf1e0221a82fcfd03174875a031276ec868f3
LAPTOP-QGQCNL3T\user <visrahandriani@gmail.com>
2021-05-20 12:28:14 +0700

    no message

--- src/spakubaaplikasi/SpakubaAplikasi.java
+++ src/spakubaaplikasi/SpakubaAplikasi.java
@@ -15,7 +15,7 @@ public class SpakubaAplikasi {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
